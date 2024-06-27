3faf55a89cbd1abc7b04340427f813f42dcb8670
林允儿 Yoona Lim <miraclefishleong@gmail.com>
2024-06-27 19:41:06 +0800

    install plugins

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 林允儿 Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-06-13 07:05:14
+ * @LastEditors: 林允儿 Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-06-27 19:34:14
+ * @FilePath: \YoonaLim.github.io\src\.vuepress\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -20,5 +28,5 @@ export default defineUserConfig({
   theme,
 
   // Enable it with pwa
-  // shouldPrefetch: false,
+  shouldPrefetch: false,
 });
